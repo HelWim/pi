5d71285cc9b9daf14b7e6fbc9c966ab4d487ef45
HelWim <Helmut.Wimmer@inode.at>
2021-03-28 17:30:18 +0200

    dd

---
+++ T.py
@@ -0,0 +1,45 @@
+import time
+from DS18B20classFile import DS18B20
+# zum Testen der Temperatursensoren
+
+degree_sign = u'\xb0' # degree sign
+devices = DS18B20()
+count = devices.device_count()
+names = devices.device_names()
+
+Klartext='xx' # Bezeichnung der Tempsensoren
+
+
+print('[press ctrl+c to end the script]')
+try: # Main program loop
+	while True:
+		i = 0
+		print('\nReading temperature, number of sensors: {}'
+							.format(count))
+
+		while i < count:
+			container = devices.tempC(i)
+			if names[i]=='28-3c01d075619c':
+				Klartext='Warmwasser'
+			if names[i]=='28-3c01d075af0e':
+				Klartext='P-Kalt'
+			if names[i]=='28-3c01d075bdf3':
+				Klartext='P-heiss'
+			if names[i]=='28-012033389e18':
+				Klartext='Raumtemperatur'
+
+			print(i," ",container,names[i],Klartext)
+			#print (devices.tempC(0),devices.tempC(1))
+			#print (container)
+			"""
+			print('{}. Temp: {:.3f}{}C, {:.3f}{}F of the device {}'
+			  .format(i+1, container, degree_sign,
+			  container * 9.0 / 5.0 + 32.0, degree_sign,names[i]))
+			"""
+			Klartext='xx'
+			i = i + 1
+	time.sleep(1)
+
+# Scavenging work after the end of the program
+except KeyboardInterrupt:
+	print('Script end!')
